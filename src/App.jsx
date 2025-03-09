79dc5e9b727e13e66982cf1ad44f06ea426be688
j-s0-4 <thom.js04@gmail.com>
2025-03-09 11:57:55 -0600

    with function !const

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {ProgressBar} from "./ProgressBar";
+import ProgressBar from './ProgressBar';
 
 function App() {
   return (
--- src/ProgressBar.jsx
+++ src/ProgressBar.jsx
@@ -1,35 +1,37 @@
 import React, {useState} from 'react'
 
-export const ProgressBar = () => {
-    const [progress, setProgress] = useState(0);
-    const handleButtonClick = () => {
-        if(progress < 100){
-            setProgress(progress + 20);
-        }
-    };
-    const handleButtonReset = () => {
-        setProgress(0);
-    };
-
-    const getColor = () => {
-        if(progress < 40) {
-            return "#ff0000"
-        } else if(progress < 70){
-            return "#ffaa00"
-        } else {
-            return "#2ecc71";
-        }
-    }
+function ProgressBar() {
+  const [progress, setProgress] = useState(0);
 
   return (
     <div className="container">
         <div className="progress-bar">
-            <div className="progress-bar-fill" style={{width: `${progress}%`, background: getColor()}}></div>
+            <div className="progress-bar-fill" style={{width: `${progress}%`, background: getColor(progress)}}></div>
         </div>
         <div className="progress-label">{progress}%</div>
-        <button onClick={handleButtonClick}>Progress</button>
-        <button onClick={handleButtonReset}>Reset</button>
+        <button onClick={()=>handleButtonClick(progress,setProgress)}>Progress</button>
+        <button onClick={()=>handleButtonReset(setProgress)}>Reset</button>
     </div>
   );
+}
+export default ProgressBar;
+
+function handleButtonClick (progress, setProgress) {
+    if(progress < 100){
+        setProgress(progress + 20);
+    }
+};
+function handleButtonReset (setProgress)  {
+    setProgress(0);
+};
+
+const getColor = (progress) => {
+    if(progress < 40) {
+        return "#ff0000"
+    } else if(progress < 70){
+        return "#ffaa00"
+    } else {
+        return "#2ecc71";
+    }
 }
 
